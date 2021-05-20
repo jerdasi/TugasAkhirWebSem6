7f16a68945674a598c01260e4824688b8f5b98f7
jerdasi <jeremiadansil@gmail.com>
2021-05-20 22:59:25 +0700

    Pembaruan

---
+++ models/description.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const descriptionSchema = new Schema( {
+    description: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+const Description = mongoose.model('Description', descriptionSchema)
+module.exports = Description
